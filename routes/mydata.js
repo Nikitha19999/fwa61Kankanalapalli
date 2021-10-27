72560572a72b33e22be21d38bdedfe69d487101e
Nikitha19999 <nikithavyshnavi08@gmail.com>
2021-10-27 14:06:03 -0500

    new endpoint

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Nikitha Srivyshnavi Kankanalapalli' });
+  res.render('index', { title: 'Nikitha Srivyshnavi Kankanalapalli' });
 });
 
 module.exports = router;
